b6420c9463d2f26fec57ffcd3a30ea41d2b5c0a9
harverino <sabrinapscampos@gmail.com>
2023-08-25 22:09:14 -0300

    Implemented Kabum Web Scraping for AMD Products

--- oficina_web_scraping.py
+++ oficina_web_scraping.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 fabricantes = "amd"
-#fabricantes = ["amd", "intel", "nvidia"]
 sessao = requests.session()
 anuncios_obtidos = []
 
@@ -14,34 +13,29 @@ def iniciar_scraper():
     if anuncios_obtidos:
         extrair()
 
-
 def procurar():
     numero_pagina_atual = 1
-    pagina_produtos = obter_pagina_anuncios(fabricante, numero_pagina_atual)
-    anuncios = pagina_produtos["anuncios"]
-    ultima_pagina = pagina_produtos["ultima_pagina"]
+    while True:
+        pagina_produtos = obter_paginas_anuncios(fabricantes, numero_pagina_atual)
+        anuncios = pagina_produtos["anuncios"]
+        ultima_pagina = pagina_produtos["ultima_pagina"]
+        
+        for anuncio in anuncios:
+            url_produtos = anuncio.select("a")[0].attrs["href"]
+            anuncios_obtidos.append(f"https://www.kabum.com.br{url_produtos}")
+            
+        if ultima_pagina == "true":
+            break
+        
+        numero_pagina_atual += 1
 
-
-while True:
-for anuncio in anuncios:
-    url_produtos = anuncio.select("a")[0].attrs["href"]
-    anuncios_obtidos.append(f"www.kabum.com.br{url_produtos}")
-   
-    if ultima_pagina == "true" :
-    break 
-
-    numero_pagina_atual += 1 
-    pagina_parseada = obter_paginas_anuncios(fabricantes, numero_pagina_atual)
-    anuncios = proxima_pagina["anuncios"]
-    ultima_pagina = proxima_pagina["ultima_pagina"]
-
-def obter_paginas_anuncios(fabricantes, numero_pagina_atual):
-    url_produtos = f"https://www.kabum.com.br/hardware/placa-de-video-vga/placa-de-video-{fabricante}?page_number={numero_da_pagina_atual}&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjEzOSwibWF4Ijo0NDU4My45fX0=&sort=most_searched"
+def obter_paginas_anuncios(fabricante, numero_pagina_atual):
+    url_produtos = f"https://www.kabum.com.br/hardware/placa-de-video-vga/placa-de-video-{fabricante}?page_number={numero_pagina_atual}&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjEzOSwibWF4Ijo0NDU4My45fX0=&sort=most_searched"
     pagina_produtos = sessao.get(url_produtos)
     pagina_parseada = BeautifulSoup(pagina_produtos.text, "html.parser")
-    anuncios = pagina_parseada.select(."productCard")
-    ultima_pagina = pagina_parseada.select(".nextLink")[0].attrs["aria.disabled"]
-    return ("anuncios": anuncios, "ultima_pagina": ultima_pagina)
+    anuncios = pagina_parseada.select(".productCard")
+    ultima_pagina = pagina_parseada.select(".nextLink")[0].attrs["aria-disabled"]
+    return {"anuncios": anuncios, "ultima_pagina": ultima_pagina}
 
 def extrair():
     qtd_extraidos = 0
@@ -50,17 +44,28 @@ def extrair():
         pagina_anuncio = sessao.get(url_anuncio)
         pagina_parseada = BeautifulSoup(pagina_anuncio.text, "html.parser")
 
-    titulo = pagina_parseada.select("dDYTAu")[0].text
-    a_vista = pagina_parseada.select("finalPrice")[0].text
-    a_prazo = pagina_parseada.select("regularPrice")[0].text
+        titulo = pagina_parseada.select(".dDYTAu")[0].text
+        a_vista = pagina_parseada.select(".finalPrice")[0].text
 
-    pass
-                        
-                
+        try:
+            a_prazo = pagina_parseada.select(".regularPrice")[0].text
+        except Exception:
+            a_prazo = "Consultar valor."
+        
+        titulo = titulo.strip()
+        a_vista = corrigir_valor_monetario(a_vista)
+        a_prazo = corrigir_valor_monetario(a_prazo)
+
+        print(f"Produto: {titulo}")
+        print(f"Preço à vista: {a_vista}")
+        print(f"Preço a prazo: {a_prazo}")
+        print("=" * 40)
+        
+        qtd_extraidos += 1
+
 def corrigir_valor_monetario(valor_original):
     valor_original = valor_original.replace('Â', '').replace('\xa0', ' ')
     valor_corrigido = valor_original.encode('iso-8859-1').decode('utf-8')
     return valor_corrigido.replace("R$ ", "").replace(".", "").replace(",", ".")
-
 
 iniciar_scraper()
